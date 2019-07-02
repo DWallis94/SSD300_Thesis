f12aa970642abbc5ee0d044ccb630bfcfe374ffe
Danny94W <daniel.b.wallis@outlook.com>
2019-07-02 16:38:13 +1000

    Converted previous net layers to keras Layers!
    Still need to fix rest of code which interacts with the model to interpret the Keras format correctly

--- net/ssd_net_high.py
+++ net/ssd_net_high.py
@@ -56,9 +56,9 @@ _DROPOUT_RATE = 0.5
 
 class Conv2D_layer(tf.keras.layers.Layer):
 
-    def __init__(self, output_dim, kernel_size, axis, filters, filter, strides, padding, use_cudnn_on_gpu,
+    def __init__(self, kernel_size, axis, filters, strides, padding, use_cudnn_on_gpu,
                         data_format, dilations, name, **kwargs):
-        self.output_dim = output_dim
+        #self.output_dim = output_dim
         self._kernel_size = kernel_size
         self._axis = axis
         self._filters = filters
@@ -77,6 +77,7 @@ class Conv2D_layer(tf.keras.layers.Layer):
         tf.summary.histogram( "weights", conv_filter )
         conv = tf.nn.conv2d(input=x, filter=self._filter, strides=self._strides, padding=self._padding, use_cudnn_on_gpu=self._use_cudnn_on_gpu,
                             data_format=self._data_format, dilations=self._dilations, name=self._name)
+        tf.summary.histogram( "act", conv )
         return conv
 
     def build(self, input_shape):
@@ -91,8 +92,8 @@ class Conv2D_layer(tf.keras.layers.Layer):
 
 class Bias_add_layer(tf.keras.layers.Layer):
 
-    def __init__(self, output_dim, bias, data_format, **kwargs):
-        self.output_dim = output_dim
+    def __init__(self, bias, data_format, **kwargs):
+        #self.output_dim = output_dim
         self._data_format = data_format
         self._bias = bias
         super(Bias_add_layer, self).__init__(**kwargs)
@@ -133,8 +134,8 @@ class L2Normalization(tf.keras.layers.Layer):
             return tf.multiply(x, x_inv_norm, name=name)
     '''
 
-    def __init__(self, output_dim, name, feature_scale, training, data_format, **kwargs):
-        self.output_dim = output_dim
+    def __init__(self, name, feature_scale, training, data_format, **kwargs):
+        #self.output_dim = output_dim
         self._data_format = data_format
         self._name = name
         self._axis = -1 if self._data_format == 'channels_last' else 1
@@ -202,48 +203,81 @@ class VGG16Backbone(object):
         else:
             self._weight_scale = tf.reshape(self._weight_scale, [1, -1, 1, 1], name='reshape')
 
-        self._model = tf.keras.Sequential([
-            self.conv_block(64, 3, (1, 1, 1, 1), 'conv1_1', feature_scale),
-            self.conv_block(64, 3, (1, 1, 1, 1), 'conv1_2', feature_scale),
-            self._pool1,
-            self.conv_block(128, 3, (1, 1, 1, 1), 'conv2_1', feature_scale),
-            self.conv_block(128, 3, (1, 1, 1, 1), 'conv2_2', feature_scale),
-            self._pool1,
-            self.conv_block(256, 3, (1, 1, 1, 1), 'conv3_1', feature_scale),
-            self.conv_block(256, 3, (1, 1, 1, 1), 'conv3_2', feature_scale),
-            self.conv_block(256, 3, (1, 1, 1, 1), 'conv3_3', feature_scale),
-            self._pool1,
-            self.conv_block(512, 3, (1, 1, 1, 1), 'conv4_1', feature_scale),
-            self.conv_block(512, 3, (1, 1, 1, 1), 'conv4_2', feature_scale),
-            self.conv_block(512, 3, (1, 1, 1, 1), 'conv4_3', feature_scale),
+        self._model = tf.keras.Sequential()
+        with tf.variable_scope("conv1"):
+            for op in self.conv_block(64, 3, (1, 1, 1, 1), 'conv1_1', feature_scale):
+                self._model.add(op)
+            for op in self.conv_block(64, 3, (1, 1, 1, 1), 'conv1_2', feature_scale):
+                self._model.add(op)
+        self._model.add(self._pool1)
+        with tf.variable_scope("conv2"):
+            for op in self.conv_block(128, 3, (1, 1, 1, 1), 'conv2_1', feature_scale):
+                self._model.add(op)
+            for op in self.conv_block(128, 3, (1, 1, 1, 1), 'conv2_2', feature_scale):
+                self._model.add(op)
+        self._model.add(self._pool1)
+        with tf.variable_scope("conv3"):
+            for op in self.conv_block(256, 3, (1, 1, 1, 1), 'conv3_1', feature_scale):
+                self._model.add(op)
+            for op in self.conv_block(256, 3, (1, 1, 1, 1), 'conv3_2', feature_scale):
+                self._model.add(op)
+            for op in self.conv_block(256, 3, (1, 1, 1, 1), 'conv3_3', feature_scale):
+                self._model.add(op)
+            self._model.add(self._pool1)
+        with tf.variable_scope("conv4"):
+            for op in self.conv_block(512, 3, (1, 1, 1, 1), 'conv4_1', feature_scale):
+                self._model.add(op)
+            for op in self.conv_block(512, 3, (1, 1, 1, 1), 'conv4_2', feature_scale):
+                self._model.add(op)
+            for op in self.conv_block(512, 3, (1, 1, 1, 1), 'conv4_3', feature_scale):
+                self._model.add(op)
             # conv4_3
-            L2Normalization(name='norm', feature_scale=feature_scale, training=training, data_format=self._data_format),
-            self._pool1,
-            self.conv_block(512, 3, (1, 1, 1, 1), 'conv5_1', feature_scale),
-            self.conv_block(512, 3, (1, 1, 1, 1), 'conv5_2', feature_scale),
-            self.conv_block(512, 3, (1, 1, 1, 1), 'conv5_3', feature_scale),
-            self._pool5,
+        with tf.variable_scope("conv4_3_scale"):
+            self._model.add(L2Normalization(name='norm', feature_scale=feature_scale, training=training, data_format=self._data_format))
+        self._model.add(self._pool1)
+        with tf.variable_scope("conv5"):
+            for op in self.conv_block(512, 3, (1, 1, 1, 1), 'conv5_1', feature_scale):
+                self._model.add(op)
+            for op in self.conv_block(512, 3, (1, 1, 1, 1), 'conv5_2', feature_scale):
+                self._model.add(op)
+            for op in self.conv_block(512, 3, (1, 1, 1, 1), 'conv5_3', feature_scale):
+                self._model.add(op)
+        self._model.add(self._pool5)
             # forward fc layers
-
-            self.conv_block(filters=1024, feature_scale=feature_scale, kernel_size=3, strides=[1,1,1,1], padding='SAME', dilations=self._dilation6,
-                                    activation=tf.nn.relu, batch_norm=False, use_bias=True, name='fc6', reuse=None),
-            self.conv_block(filters=1024, feature_scale=feature_scale, kernel_size=1, strides=[1,1,1,1], padding='SAME',
-                                    activation=tf.nn.relu, batch_norm=False, use_bias=True, name='fc7', reuse=None),
+        with tf.variable_scope("conv6"):
+            for op in self.conv_block(filters=1024, feature_scale=feature_scale, kernel_size=3, strides=[1,1,1,1], padding='SAME', dilations=self._dilation6,
+                                    activation=tf.nn.relu, batch_norm=False, use_bias=True, name='fc6', reuse=None):
+                                    self._model.add(op)
+            for op in self.conv_block(filters=1024, feature_scale=feature_scale, kernel_size=1, strides=[1,1,1,1], padding='SAME',
+                                    activation=tf.nn.relu, batch_norm=False, use_bias=True, name='fc7', reuse=None):
+                                    self._model.add(op)
 
             # forward ssd layers
-            self.conv_block(filters=256, feature_scale=feature_scale, kernel_size=1, strides=(1, 1, 1, 1), use_bias=True, name='conv8_1'),
-            self.conv_block(filters=512, feature_scale=feature_scale, kernel_size=3, strides=self._stride8, use_bias=True, name='conv8_2'),
+        with tf.variable_scope("additional_layers"):
+            with tf.variable_scope("conv8"):
+                for op in self.conv_block(filters=256, feature_scale=feature_scale, kernel_size=1, strides=(1, 1, 1, 1), use_bias=True, name='conv8_1'):
+                    self._model.add(op)
+                for op in self.conv_block(filters=512, feature_scale=feature_scale, kernel_size=3, strides=self._stride8, use_bias=True, name='conv8_2'):
+                    self._model.add(op)
+            with tf.variable_scope("conv9"):
             # conv8
-            self.conv_block(filters=128, feature_scale=feature_scale, kernel_size=1, strides=(1, 1, 1, 1), use_bias=True, name='conv9_1'),
-            self.conv_block(filters=256, feature_scale=feature_scale, kernel_size=3, strides=self._stride9, use_bias=True, name='conv9_2'),
+                for op in self.conv_block(filters=128, feature_scale=feature_scale, kernel_size=1, strides=(1, 1, 1, 1), use_bias=True, name='conv9_1'):
+                    self._model.add(op)
+                for op in self.conv_block(filters=256, feature_scale=feature_scale, kernel_size=3, strides=self._stride9, use_bias=True, name='conv9_2'):
+                    self._model.add(op)
+            with tf.variable_scope("conv10"):
             # conv9
-            self.conv_block(filters=128, feature_scale=feature_scale, kernel_size=1, strides=(1, 1, 1, 1), use_bias=True, name='conv10_1', padding='VALID'),
-            self.conv_block(filters=256, feature_scale=feature_scale, kernel_size=3, strides=(1, 1, 1, 1), use_bias=True, name='conv10_2', padding='VALID'),
+                for op in self.conv_block(filters=128, feature_scale=feature_scale, kernel_size=1, strides=(1, 1, 1, 1), use_bias=True, name='conv10_1', padding='VALID'):
+                    self._model.add(op)
+                for op in self.conv_block(filters=256, feature_scale=feature_scale, kernel_size=3, strides=(1, 1, 1, 1), use_bias=True, name='conv10_2', padding='VALID'):
+                    self._model.add(op)
+            with tf.variable_scope("conv11"):
             # conv10
-            self.conv_block(filters=128, feature_scale=feature_scale, kernel_size=1, strides=(1, 1, 1, 1), use_bias=True, name='conv11_1', padding='VALID'),
-            self.conv_block(filters=256, feature_scale=feature_scale, kernel_size=3, strides=(1, 1, 1, 1), use_bias=True, name='conv11_2', padding='VALID')
+                for op in self.conv_block(filters=128, feature_scale=feature_scale, kernel_size=1, strides=(1, 1, 1, 1), use_bias=True, name='conv11_1', padding='VALID'):
+                    self._model.add(op)
+                for op in self.conv_block(filters=256, feature_scale=feature_scale, kernel_size=3, strides=(1, 1, 1, 1), use_bias=True, name='conv11_2', padding='VALID'):
+                    self._model.add(op)
             # conv11
-        ])
 
     def forward(self):
         return self._model
@@ -332,18 +366,16 @@ class VGG16Backbone(object):
             conv_ops = []
             data_format = "NHWC" if self._data_format == 'channels_last' else "NCHW"
             bias = tf.get_variable('bias', filters)
-            conv_ops.append(Conv2D_layer(kernel_shape=kernel_size, axis=self._bn_axis, filters=filters, filter=conv_filter,strides=strides,padding=padding,use_cudnn_on_gpu=True,
-                                data_format=data_format,dilations=dilations,name=name))
-            tf.summary.histogram( "act", conv )
+            conv_ops.append(Conv2D_layer(kernel_size=kernel_size, axis=self._bn_axis, filters=filters, strides=strides, padding=padding, use_cudnn_on_gpu=True,
+                                data_format=data_format, dilations=dilations,name=name))
             conv_ops.append(Bias_add_layer(bias=bias, data_format=data_format))
             if batch_norm:
-                conv_ops.append(tf.keras.layers.batch_normalization(axis=self._bn_axis, momentum=_BATCH_NORM_DECAY, epsilon=_BATCH_NORM_EPSILON, fused=_USE_FUSED_BN,
-                        reuse=None))
+                conv_ops.append(tf.keras.layers.BatchNormalization(axis=self._bn_axis, momentum=_BATCH_NORM_DECAY, epsilon=_BATCH_NORM_EPSILON, fused=_USE_FUSED_BN))
             else:
                 conv_ops.append(tf.keras.layers.Dropout(rate=_DROPOUT_RATE))
-            tf.summary.histogram( "act_bn", conv )
-            conv_ops.append(tf.keras.layers.ReLu())
-            tf.summary.histogram( "act_bn_r", conv )
+            #tf.summary.histogram( "act_bn", conv_ops[-1] )
+            conv_ops.append(tf.keras.layers.ReLU())
+            #tf.summary.histogram( "act_bn_r", conv_ops[-1] )
             return conv_ops
 
     '''
@@ -432,12 +464,12 @@ def multibox_head(feature_layers, num_classes, num_anchors_depth_per_layer, data
         cls_preds = []
         loc_preds = []
         for ind, feat in enumerate(feature_layers.layers):
-            loc_preds.append(tf.layers.conv2d(feat.output, num_anchors_depth_per_layer[ind] * 4, (3, 3), use_bias=True,
+            loc_preds.append(tf.layers.conv2d(feat, num_anchors_depth_per_layer[ind] * 4, (3, 3), use_bias=True,
                         name='loc_{}'.format(ind), strides=(1, 1),
                         padding='same', data_format=data_format, activation=None,
                         kernel_initializer=tf.glorot_uniform_initializer(),
                         bias_initializer=tf.zeros_initializer()))
-            cls_preds.append(tf.layers.conv2d(feat.output, num_anchors_depth_per_layer[ind] * num_classes, (3, 3), use_bias=True,
+            cls_preds.append(tf.layers.conv2d(feat, num_anchors_depth_per_layer[ind] * num_classes, (3, 3), use_bias=True,
                         name='cls_{}'.format(ind), strides=(1, 1),
                         padding='same', data_format=data_format, activation=None,
                         kernel_initializer=tf.glorot_uniform_initializer(),
